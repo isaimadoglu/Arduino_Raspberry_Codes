fa12b36feb94938068d61672edfc5ba3b1ca85ca
Ömer Faruk ESEN <41272882+omer-faruk-esen@users.noreply.github.com>
2019-09-20 04:02:26 +0300

    Update StepperControlWithPotOnArduino.ino

--- StepperControlWithPotOnArduino/StepperControlWithPotOnArduino.ino
+++ StepperControlWithPotOnArduino/StepperControlWithPotOnArduino.ino
@@ -1,27 +1,27 @@
 
 #include <Stepper.h>
 
-#define motorSteps 200     // change this depending on the number of steps
-                           // per revolution of your motor
+#define motorAdimi 200     
+                           
 #define motorPin1 8
 #define motorPin2 9
 #define motorPin3 10
 #define motorPin4 11
 #define potPin A0
-#define ledPin 13
-int forward=200;
+
+int ileri=200;
 int potValue=0;
 
-// initialize of the Stepper library:
-Stepper myStepper(motorSteps, motorPin1,motorPin2,motorPin3,motorPin4); 
+
+Stepper stepMotor(motorAdimi, motorPin1,motorPin2,motorPin3,motorPin4); 
 
 void setup() {
   
-  // set the motor speed at 60 RPMS:
-  myStepper.setSpeed(60);
+  
+  stepMotor.setSpeed(60);
   pinMode(potPin,INPUT);
   
-  // Initialize the Serial port:
+  
   Serial.begin(9600);
 
   
@@ -36,7 +36,7 @@ void loop() {
   }
   Serial.println(potValue);
   // Step forward 100 steps:
-  myStepper.step(forward);
-  delay(potValue);
+  stepMotor.step(ileri);
+  
 
 }
